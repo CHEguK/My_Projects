de283f697cd074561727bcb4b524d7c3fc024b06
CHEguK <zdrogov.an@yandex.ru>
2019-02-05 21:23:59 +0500

    create project

---
+++ sweeper/src/sweeper/Bomb.java
@@ -0,0 +1,60 @@
+package sweeper;
+
+import java.util.Random;
+
+class Bomb
+{
+    private Matrix bombMap;
+    private  int totlaBobmbs;
+
+    Bomb (int totlaBobmbs)
+    {
+        this.totlaBobmbs = totlaBobmbs;
+        fixBombsCount();
+    }
+
+    void start()
+    {
+        bombMap = new Matrix(Box.ZERO);
+        for (int j=0; j<totlaBobmbs; j++)
+            placeBomb();
+    }
+
+    Box get (Coord coord)
+    {
+        return bombMap.get(coord);
+    }
+
+    private void fixBombsCount()
+    {
+        int maxBombs = Ranges.getSize().x * Ranges.getSize().y/2;
+        if (totlaBobmbs > maxBombs)
+            totlaBobmbs = maxBombs;
+    }
+
+    private void placeBomb()
+    {
+        while(true)
+        {
+            Coord coord = Ranges.getRandomCoord();
+            if(Box.BOMB == bombMap.get(coord))
+                continue;
+            bombMap.set(coord, Box.BOMB);
+            incNumbersAroundBomb(coord);
+            break;
+
+        }
+
+    }
+    private void incNumbersAroundBomb (Coord coord)
+    {
+        for (Coord around : Ranges.getCoordAround(coord))
+            if (Box.BOMB != bombMap.get (around))
+                bombMap.set (around, bombMap.get(around).getNextNumberBox());
+    }
+
+    int getTotalBombs()
+    {
+        return totlaBobmbs;
+    }
+}
